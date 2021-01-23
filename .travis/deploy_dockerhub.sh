9e8676690494f1bcd699b235c15df577a766fa24
samalpramod <samalpramod@hotmail.com>
2021-01-24 01:28:03 +0530

    Step 9

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,10 +1,9 @@
-#! /bin/sh
-docker login -u$DOCKER_USER - p$DOCKER_PASS
-if ["$TRAVIS_BRANCH" = "master"]; then
-	TAG = "latest"
+#!/bin/sh
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
 else
-	TAG = "$TRAVIS_BRANCH"
+    TAG="$TRAVIS_BRANCH"
 fi
-
-docker build -f Dockerfile -t $TRAVIS_REPOSLUG:$TAG
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG:$TAG
