1b426a233d17280179ee2667eb271ed20a6079ff
samalpramod <samalpramod@hotmail.com>
2021-01-25 01:22:28 +0530

    Type correction

--- buzz/generate_buzz.py
+++ buzz/generate_buzz.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import random
 
 buzz = ('continuous testing','continuous integration',
-	'continuous deplyment','continuous improvement','devops')
+	'continuous deployment','continuous improvement','devops')
 adjectives = ('complete','modern','self-service','integrated','end-to-end')
 adverbs = ('remarkably','enormously','substantially','significantly','seriously')
 verbs = ('accelerates','improves','enhances','revamps','boosts')
