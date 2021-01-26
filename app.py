5960a6660afa25b3dc783a9d7089e45c5dd148f2
samalpramod <samalpramod@hotmail.com>
2021-01-26 22:01:04 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -10,13 +10,9 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate():
-	#page = "<html><body><h1>"
-	#page += generate_buzz.generate_buzz()
-	#page += "</h1></body></html>"
 	buzz = generate_buzz.generate_buzz()
 	return render_template('index.html',buzz=buzz)
 	
-	return page
 	
 if __name__ == "__main__":
 	#app.run(host='0.0.0.0',port=int(os.getenv('PORT',5000)))
